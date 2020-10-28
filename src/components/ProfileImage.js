143ff99be04a1aff0f37b9ee44fa63096d7e5662
olafk <olaf.koziara@gmail.com>
2020-10-28 17:29:56 +0100

    Update ProfileImage.js

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { StyledProfileImage } from "../styledComponents";
+
+const ProfileImage = ({ name, surname, email, imageUrl }) => {
+  return (
+    <Link to={{ pathname: `/${name}_${surname}`, state: { email: email } }}>
+      <StyledProfileImage src={imageUrl} />
+    </Link>
+  );
+};
+
+export default ProfileImage;
