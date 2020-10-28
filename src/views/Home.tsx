ad8eb25a5f7919429af2b62147ae073b6c7bb1fc
olafk <olaf.koziara@gmail.com>
2020-10-28 17:30:37 +0100

    Post creatore added to home view

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PostCreatore from '../components/PostCreatore'
 
 const Home = () => {
     return (
         <div>
-           
+          <PostCreatore/> 
         </div>
     )
 }
