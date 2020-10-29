ca215872a04eeda452e850f778f1c977531e7dfc
olafk <olaf.koziara@gmail.com>
2020-10-29 23:17:59 +0100

    custom font size props

--- src/components/atoms/Input.js
+++ src/components/atoms/Input.js
@@ -4,6 +4,11 @@ export const Input = styled.input`
   border: 0;
   border-bottom: 1px solid white;
   color: white;
-  font-size: 20px;
+  font-size: ${({ fontSize }) => `${fontSize}px`};
+  min-width: 250px;
+  &::placeholder {
+    font-size: 15px;
+  }
+
   outline: none;
 `;
