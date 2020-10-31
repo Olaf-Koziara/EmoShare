421d451b31df5596586e6d9eb86b8178c8db8894
olafk <olaf.koziara@gmail.com>
2020-10-31 12:12:15 +0100

    Update PostsList.js

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { firestore } from "../firebaseConfig";
+import { StyledPostsList } from "../styledComponents";
+import PostListItem from "./PostListItem";
 
-const PostsList = ({ posts }) => {
-  return (
-    <ul>
-      {posts.map((post) => (
-        <li>{post.content}</li>
-      ))}
-    </ul>
-  );
+const PostsList = ({ posts, user, images }) => {
+  const _postsRender = posts.map((post) => {
+    const url = images.find((image) => image.name === post.imageName);
+    console.log(url);
+    if (url) {
+      return (
+        <PostListItem
+          imageUrl={url.url}
+          own={user.email === post.email}
+          post={post}
+        />
+      );
+    }
+  });
+  return <StyledPostsList>{images ? _postsRender : null}</StyledPostsList>;
 };
 const mapStateToProps = (state) => ({
   posts: state.posts,
+  user: state.user,
+  images: state.profileImagesUrl,
 });
 export default connect(mapStateToProps)(PostsList);
