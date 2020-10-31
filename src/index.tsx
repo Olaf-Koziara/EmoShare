67236a3d4e4faec9141c44e19c49107d4b517478
olafk <olaf.koziara@gmail.com>
2020-10-31 12:12:47 +0100

    from ts to js for root

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 import * as serviceWorker from "./serviceWorker";
-import Root from "./Root";
+import Root from "./Root.js";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
