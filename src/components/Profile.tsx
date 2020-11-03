a0dc9f16a165c850069c9023eee13011851d419f
olafk <olaf.koziara@gmail.com>
2020-11-03 22:36:29 +0100

    follow user functionality added

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { storage } from '../firebaseConfig'
+import { connect } from 'react-redux'
+import { firestore, storage } from '../firebaseConfig'
 import { StledProfileTextParagraph, StyledProfileImage, StyledProfileImageWrapper, StyledProfileTextWrapper, StyledProfileWrapper } from '../styledComponents'
-type propsType = {user:any}
-const Profile = ({user}:propsType) => {
+type propsType = {user:any,followUser:any,userDocumentId:string,userFollows:any,usersImages:any}
+const Profile = ({user,followUser,userDocumentId,userFollows,usersImages}:propsType) => {
     const[profileImage,setProfileImage] = useState<string>();
     const getImage = (imageName:string)=>{
         console.log(imageName)
-
-        const pathRef = storage.ref("/photos/"+imageName);
-       
-        pathRef.getDownloadURL().then((url)=> {setProfileImage(url) });
+        console.log(usersImages)
+        const userImage = usersImages.find((object:any)=>object.name===user.email);
+        if(userImage){
+        setProfileImage(userImage.url)
+        }
     
         
       
     }
-    useEffect(()=>{getImage(user.profileImage)},[])
+    const follow = (email:string)=>{
+        firestore.collection("users").doc(userDocumentId).update({follows:[...userFollows,email]})
+    }
+    
+    useEffect(()=>{getImage(user.profileImage)},[usersImages])
     const birthDate = new Date(user.birthDate);
     
     return (
@@ -23,8 +29,14 @@ const Profile = ({user}:propsType) => {
            <StyledProfileImage mxAuto src={profileImage}  large />
            </StyledProfileImageWrapper>
            <StyledProfileTextWrapper><StledProfileTextParagraph>Name:{user.name}</StledProfileTextParagraph><StledProfileTextParagraph>Surname:{user.surname}</StledProfileTextParagraph><StledProfileTextParagraph>BirthDate:{`${birthDate.getDate()}-${birthDate.getMonth()+1}-${birthDate.getFullYear()}`}</StledProfileTextParagraph></StyledProfileTextWrapper>
+           <button onClick={()=>follow(user.email)}>Follow</button>
         </StyledProfileWrapper>
     )
 }
+const mapStateToProps = (state:any)=>({
+    userDocumentId:state.user.id,
+    userFollows:state.user.follows,
+    usersImages:state.profileImagesUrl
+  })
 
-export default Profile
+export default connect(mapStateToProps)(Profile)
