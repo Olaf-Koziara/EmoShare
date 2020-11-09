f0078118eb98dda9dd1f255bfe21dd22b80d8684
olafk <olaf.koziara@gmail.com>
2020-11-09 17:29:47 +0100

    Update SearchList.js

--- src/components/search/SearchList.js
+++ src/components/search/SearchList.js
@@ -2,14 +2,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import { StyledSearchList } from "../../styledComponents";
+import SearchListItem from "./SearchListItem";
 
 const SearchList = () => {
   const users = useSelector((state) => state.searchActiveUsers);
   return (
     <StyledSearchList>
-      {users.map((user) => (
-        <li>{user.name}</li>
-      ))}
+      {users.map((user, index) =>
+        user ? <SearchListItem key={index} user={user} /> : null,
+      )}
     </StyledSearchList>
   );
 };
