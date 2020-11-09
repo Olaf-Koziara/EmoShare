e49657a9b1bcee8e1e3a3744efd931115d446d8d
olafk <olaf.koziara@gmail.com>
2020-11-09 17:29:42 +0100

    Update SearchListItem.js

--- src/components/search/SearchListItem.js
+++ src/components/search/SearchListItem.js
@@ -4,9 +4,16 @@ import { StyledSearchListItem } from "../../styledComponents";
 import ProfileImageLink from "../ProfileImageLink";
 
 const SearchListItem = ({ user }) => {
+  const { profileImage, email, name, surname } = user;
   return (
     <StyledSearchListItem>
-      <ProfileImageLink></ProfileImageLink>
+      <ProfileImageLink
+        imageUrl={profileImage}
+        email={email}
+        name={name}
+        surname={surname}
+      />
+      <p>{`${user.name} ${user.surname}`}</p>
     </StyledSearchListItem>
   );
 };
