502efabeeb448614eba61c3725cfd8edf090acf3
olafk <olaf.koziara@gmail.com>
2020-11-09 17:29:49 +0100

    Update Search.js

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,20 +2,37 @@ import React, { useState } from "react";
 import { StyledNavIcon, StyledNavSearchWrapper } from "../../styledComponents";
 import { Input } from "../atoms/Input";
 import searchIcon from "../../assets/icons/001-search.png";
-import { setActiveUsersAction } from "../../actions";
+import { addImageUrlAction, setActiveUsersAction } from "../../actions";
 import { connect } from "react-redux";
-import { firestore } from "../../firebaseConfig";
+import { firestore, storage } from "../../firebaseConfig";
 import withVisiblity from "../../hoc/withVisiblity";
 import SearchList from "./SearchList";
-const Search = ({ setActiveUsers, activeUseres, isVisible, setVisibility }) => {
+import Axios from "axios";
+const Search = ({
+  setActiveUsers,
+  activeUseres,
+  isVisible,
+  setVisibility,
+  addImageUrl,
+  imagesUrls,
+}) => {
   const [users, setUsers] = useState([]);
+  Axios.get(
+    "https://emoji-api.com/categories?access_key=79a64a94f2e74f9f30b1c3b06ffbc6c6420c20c2",
+    {
+      data: { slug: "smileys-emotion" },
+    },
+  ).then((value) => console.log(value));
   const searchUsers = (e) => {
     const value = e.target.value;
     console.log(value);
     if (value.length === 0) {
+      setActiveUsers([]);
       setVisibility(false);
     } else {
-      setVisibility(true);
+      if (activeUseres.length > 0) {
+        setVisibility(true);
+      }
     }
 
     firestore
@@ -24,13 +41,19 @@ const Search = ({ setActiveUsers, activeUseres, isVisible, setVisibility }) => {
       .then((querySnapshot) => {
         let usersTemp = [];
         querySnapshot.forEach((doc) => {
-          const userNames = `${doc.data().name} ${doc.data().surname}`;
+          const user = doc.data();
+          const userNames = `${user.name} ${user.surname}`;
 
-          if (userNames.includes(value) && value !== "") {
-            usersTemp = [...usersTemp, doc.data()];
+          if (
+            userNames.toLowerCase().includes(value.toLowerCase()) &&
+            value.length >= 3
+          ) {
+            usersTemp = [...usersTemp, user];
           }
         });
-        setActiveUsers(usersTemp);
+        if (usersTemp.length > 0) {
+          setActiveUsers(usersTemp);
+        }
       });
   };
   return (
@@ -48,9 +71,11 @@ const Search = ({ setActiveUsers, activeUseres, isVisible, setVisibility }) => {
 };
 const mapStateToProps = (state) => ({
   activeUseres: state.searchActiveUsers,
+  imagesUrls: state.profileImagesUrl,
 });
 const mapDispatchToProps = (dispatch) => ({
   setActiveUsers: (value) => dispatch(setActiveUsersAction(value)),
+  addImageUrl: (url) => dispatch(addImageUrlAction(url)),
 });
 export default withVisiblity(
   connect(mapStateToProps, mapDispatchToProps)(Search),
