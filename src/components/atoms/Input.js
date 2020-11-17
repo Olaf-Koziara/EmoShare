a80c7cd8715433cff7af70fbd5def045288b1c56
olafk <olaf.koziara@gmail.com>
2020-11-17 16:33:56 +0100

    Update Input.js

--- src/components/atoms/Input.js
+++ src/components/atoms/Input.js
@@ -4,8 +4,13 @@ export const Input = styled.input`
   border: 0;
   border-bottom: 1px solid white;
   color: white;
+  padding: 5px;
   font-size: ${({ fontSize }) => `${fontSize}px`};
-  min-width: 250px;
+  margin: 4px;
+
+  @media (min-width: 900px) {
+    min-width: 250px;
+  }
   width: 100%;
   &::placeholder {
     font-size: 15px;
