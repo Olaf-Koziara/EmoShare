5e1a847e8ecbeb63b329e739289fb2683cf0d684
olafk <olaf.koziara@gmail.com>
2020-11-20 10:34:31 +0100

    Update LoginForm.tsx

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -24,8 +24,8 @@ const LoginForm = () => {
     <Formik   initialValues={initialValues} onSubmit={(e)=>handleLogin(e)} >
     
       <StyledForm >
-        <StyledField placeHolder="e-mail" name="email" type="email" />
-        <StyledField placeHolder="password" name="password" type="password" />
+        <StyledField placeholder="e-mail" name="email" type="email" />
+        <StyledField placeholder="password" name="password" type="password" />
         <Button type="submit">Login</Button>
         <Link to="/register"> <Button >Register</Button></Link>
       </StyledForm>
