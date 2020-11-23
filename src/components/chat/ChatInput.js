aba127cf8c9f0f66af957b29a5f9787ad26a141e
olafk <olaf.koziara@gmail.com>
2020-11-23 18:47:43 +0100

    Update ChatInput.js

--- src/components/chat/ChatInput.js
+++ src/components/chat/ChatInput.js
@@ -1,12 +1,41 @@
 import React from "react";
+import {
+  StyledCloseIcon,
+  StyledField,
+  StyledMessageContainer,
+  StyledMessageWrapper,
+} from "../../styledComponents";
+import { Input } from "../atoms/Input";
+import ProfileImageLink from "../ProfileImageLink";
 import "./ChatInput.css";
-const ChatInput = ({ sendMessage, uid, messages }) => {
+import closeIcon from "../../assets/icons/close.png";
+const ChatInput = ({ photo, sendMessage, messages, chatUser }) => {
+  const { name, surname, uid, chatId, profileImage } = chatUser;
   return (
     <div className="chatInputWrapper">
-      <div className="messagesWrapper"></div>
-      <form onSubmit={(e) => sendMessage(e, uid)}>
-        <input name="message" />
-        <button type="submit">Send</button>
+      <div className="borderWrapper">
+        <ProfileImageLink
+          imageUrl={profileImage}
+          uid={uid}
+        >{`${name} ${surname}`}</ProfileImageLink>
+        <StyledCloseIcon src={closeIcon} alt="close" size={"medium"} />
+      </div>
+      <div className="messagesWrapper">
+        {messages
+          ? messages.reverse().map((message) => (
+              <StyledMessageWrapper own={message.to ? true : false}>
+                <StyledMessageContainer own={message.to ? true : false}>
+                  {message.message}
+                </StyledMessageContainer>
+              </StyledMessageWrapper>
+            ))
+          : null}
+      </div>
+      <form className="textForm" onSubmit={(e) => sendMessage(e, chatId, uid)}>
+        <input className="chatTextInput" name="message" />
+        <button className="sendButton" type="submit">
+          Send
+        </button>
       </form>
     </div>
   );
