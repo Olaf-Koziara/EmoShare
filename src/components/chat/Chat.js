0eda3293a0ff11f15cc842d9c61b6b8a650d9eca
olafk <olaf.koziara@gmail.com>
2020-11-23 18:47:38 +0100

    Update Chat.js

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -7,8 +7,18 @@ import { wsUrl } from "../../utils/wsUrl";
 import ChatInput from "./ChatInput";
 
 class Chat extends Component {
-  state = { counter: 0, messages: [], chatId: "" };
-  ws = new WebSocket(wsUrl);
+  state = { counter: 0, messages: [], chatId: null };
+
+  ws = "";
+
+  componentDidUpdate(prevState) {
+    if (this.state.chatId !== prevState.chatId && this.props.user.docId) {
+      firestore
+        .collection("users")
+        .doc(this.props.user.docId)
+        .update({ chatId: this.state.chatId });
+    }
+  }
   componentWillReceiveProps(nextProps) {
     console.log("recive");
 
@@ -20,16 +30,16 @@ class Chat extends Component {
         .collection("users")
         .doc(nextProps.user.docId)
         .update({ chatId: this.state.chatId });
+
+      this.ws.onmessage = (evt) => {
+        const message = JSON.parse(evt.data);
+
+        if (message.message) {
+          console.log(message.message);
+          this.addMessage({ name: message.name, message: message.message });
+        }
+      };
     }
-
-    this.ws.onmessage = (evt) => {
-      const message = JSON.parse(evt.data);
-
-      if (message.message) {
-        console.log(message.message);
-        this.addMessage({ name: message.name, message: message.message });
-      }
-    };
     this.ws.onclose = () => {
       console.log("close");
 
@@ -43,28 +53,36 @@ class Chat extends Component {
   }
   componentDidMount() {
     this.ws = new WebSocket(wsUrl);
+    console.log("mount");
     this.ws.onmessage = (evt) => {
       const message = JSON.parse(evt.data);
       console.log(message);
       this.setState({ chatId: message.uid });
     };
   }
-
-  sendMessage = (e, uid) => {
+  componentWillUnmount() {
+    firestore
+      .collection("users")
+      .doc(this.props.user.docId)
+      .update({ chatId: null });
+    this.setState({ chatId: null });
+  }
+  sendMessage = (e, chatId, uid) => {
     e.preventDefault();
 
     const message = {
       name: this.props.user.name,
       message: e.target.message.value,
-      uid: uid,
+      uid: chatId,
     };
     console.log(message);
     this.ws.send(JSON.stringify(message));
     this.addMessage({
       name: message.name,
       message: message.message,
-      own: true,
+      to: uid,
     });
+    e.target.reset();
   };
   addMessage = (message) =>
     this.setState({ messages: [message, ...this.state.messages] });
@@ -72,13 +90,19 @@ class Chat extends Component {
     return (
       <StyledChatsWrapper>
         {this.props.selectedChats
-          ? this.props.selectedChats.map((index) => (
-              <ChatInput
-                messages={this.state.messages}
-                sendMessage={this.sendMessage}
-                uid={this.props.chatUsers[index].chatId}
-              />
-            ))
+          ? this.props.selectedChats.map((index) =>
+              this.props.chatUsers[index] ? (
+                <ChatInput
+                  chatUser={this.props.chatUsers[index]}
+                  messages={this.state.messages.filter(
+                    (message) =>
+                      message.name === this.props.chatUsers[index].name ||
+                      message.to === this.props.chatUsers[index].uid,
+                  )}
+                  sendMessage={this.sendMessage}
+                />
+              ) : null,
+            )
           : null}
       </StyledChatsWrapper>
     );
