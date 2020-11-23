79dbe334a1472728bee8d9825e12efff2bae44bc
olafk <olaf.koziara@gmail.com>
2020-11-23 18:47:45 +0100

    Update ChatSelect.js

--- src/components/chat/ChatSelect.js
+++ src/components/chat/ChatSelect.js
@@ -3,13 +3,17 @@ import { connect, useDispatch, useSelector } from "react-redux";
 import { selectChatAction } from "../../actions";
 import { firestore } from "../../firebaseConfig";
 import withVisiblity from "../../hoc/withVisiblity";
+import { CSSTransition } from "react-transition-group";
 import {
   StyledChatIcon,
   StyledChatSelectList,
   StyledChatSelectListItem,
+  StyledCloseIcon,
 } from "../../styledComponents";
 import chatIcon from "../../assets/icons/001-chat.png";
 import Chat from "./Chat";
+import exitIcon from "../../assets/icons/close.png";
+import "../../animations/index.css";
 const ChatSelect = ({ isVisible, setVisibility }) => {
   const chatUsers = useSelector((state) => state.chatUsers);
   const dispatch = useDispatch();
@@ -20,20 +24,32 @@ const ChatSelect = ({ isVisible, setVisibility }) => {
         <button onClick={() => setVisibility(!isVisible)}>
           <StyledChatIcon src={chatIcon} alt="chats" />{" "}
         </button>
-      ) : (
+      ) : null}
+      <CSSTransition
+        appear
+        timeout={200}
+        unmountOnExit
+        classNames="pop"
+        in={isVisible}
+      >
         <StyledChatSelectList>
-          {chatUsers.length > 0
-            ? chatUsers.map((user, index) => (
-                <StyledChatSelectListItem
-                  onClick={() => dispatch(selectChatAction(index))}
-                  key={index}
-                >
-                  {user.name + " " + user.surname}
-                </StyledChatSelectListItem>
-              ))
-            : null}
+          <button onClick={() => setVisibility(!isVisible)}>
+            <StyledCloseIcon size={"medium"} src={exitIcon} alt="exit" />
+          </button>
+          {chatUsers.length > 0 ? (
+            chatUsers.map((user, index) => (
+              <StyledChatSelectListItem
+                onClick={() => dispatch(selectChatAction(index))}
+                key={index}
+              >
+                {user.name + " " + user.surname}
+              </StyledChatSelectListItem>
+            ))
+          ) : (
+            <div className="text-center">No active users</div>
+          )}
         </StyledChatSelectList>
-      )}
+      </CSSTransition>
     </>
   );
 };
