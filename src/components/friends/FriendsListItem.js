629c77a81232221033f2cba42f89e40f58f5daf2
olafk <olaf.koziara@gmail.com>
2020-11-26 13:07:56 +0100

    Update FriendsListItem.js

--- src/components/friends/FriendsListItem.js
+++ src/components/friends/FriendsListItem.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { StyledProfileNameWrapper } from "../../styledComponents";
+import ProfileImageLink from "../ProfileImageLink";
 
 const FriendsListItem = ({ friend }) => {
-  const { name } = friend;
-  return <div>{name}</div>;
+  const { name, surname, profileImage, uid } = friend;
+  return (
+    <div>
+      <ProfileImageLink uid={uid} imageUrl={profileImage} size={"medium"}>
+        {" "}
+        <StyledProfileNameWrapper>
+          {name} {surname}
+        </StyledProfileNameWrapper>
+      </ProfileImageLink>
+    </div>
+  );
 };
 
 export default FriendsListItem;
